8cec10864d0855c4100487f16a4b677658ede9ab
Hikmat Fadrial Ramadhan <hikmatfadrial2000@gmail.com>
2025-01-09 19:54:43 +0700

    Using the Device Camera for Taking Pictures

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:favorite_places_app/screens/places.dart';
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
   seedColor: const Color.fromARGB(255, 102, 6, 247),
-  background: const Color.fromARGB(255, 56, 49, 66),
+  surface: const Color.fromARGB(255, 56, 49, 66),
 );
 
 final theme = ThemeData().copyWith(
